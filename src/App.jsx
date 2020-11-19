b34edaf584145bd77d1813b2b9fe458803bfc6f7
ironsocks <brach.thomasja@gmail.com>
2020-11-19 11:58:30 +0100

    delete technicians

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { Fragment, useEffect } from "react";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import AddButton from "./components/layout/AddButton";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
+
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+
+const App = () => {
+  useEffect(() => {
+    // Initialize Materialize JS
+    M.AutoInit();
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddButton />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+  );
+};
+
+export default App;
